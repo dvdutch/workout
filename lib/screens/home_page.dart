a5b7b22e0b23fd91f4538e30e7e65c90de8c76e5
David Chopin <david.chopin@wwt.com>
2023-04-16 21:35:58 -0500

    Added leg workouts

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:workout/screens/legs_page.dart';
 
 import 'chest_page.dart';
 
@@ -33,6 +34,20 @@ class HomePage extends StatelessWidget {
             ),
             const SizedBox(
               height: 8.0,
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const LegsPage()),
+                );
+              },
+              child: Text(
+                'Legs',
+                style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                      color: Colors.white,
+                    ),
+              ),
             ),
           ],
         ),
